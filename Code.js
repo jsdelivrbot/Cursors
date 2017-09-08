c7ab9d68b9ea2d8c89e52614227524e8c391fb99
voncrumb <sackboy129@gmail.com>
2017-09-08 15:19:22 +1000

    Create Code.js

---
+++ Code.js
@@ -0,0 +1,62 @@
+
+
+var setting = false;
+var delay = 1;
+
+
+var c = document.getElementById("canvasContainer");
+
+var movingEvent = [];
+
+c.addEventListener("mousedown", mover)
+
+function set(){
+	setting = true;
+}
+
+function clicker(){
+	
+}
+function mover(e){
+	if(setting == true){
+		movingEvent.push(e);
+	}
+}
+
+function stop(){
+	setting = false;
+}
+
+function reset(){
+	movingEvent = [];
+}
+
+
+function doit(){
+	var i = 0;
+	var moving = false;
+	
+	if(moving == false){
+		moving = true;
+	}else{
+		moving = false;
+	}
+	move();
+function move(){
+	
+	
+	if(moving == true){
+	if(i == movingEvent.length){
+		i = 0;
+	}
+	c.dispatchEvent(movingEvent[i]);
+	i++
+	
+		setTimeout(move, delay);
+	}
+	
+}
+}
+
+
+
